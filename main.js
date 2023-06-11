673b324556b69b580778c45d7ea1e322a0efaaaa
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-06-11 17:55:24 -0500

    forEach

--- main.js
+++ main.js
@@ -19,11 +19,34 @@
 
 // const mayorQueDiez = num =>  num > 10;
 
-function mayorQue(x) {
-  return (num) => num > x;
-}
+// function mayorQue(x) {
+//   return (num) => num > x;
+// }
 
-const mayorQueDiez = mayorQue(10);
-const mayoQueVeinte = mayorQue(20);
-console.log(mayorQueDiez(8));
+// const mayorQueDiez = mayorQue(10);
+// const mayoQueVeinte = mayorQue(20);
+// console.log(mayorQueDiez(8));
+
+/* Metodos de busqueda */
+
+const numeros = [50, 12, 37, 17, 25, 6];
+const animales = ["perro", "gato", "cisne", "tortuga"];
+const productos = [
+  {nombre : "mesa", precio : 35},
+  {nombre : "silla de plastico", precio : 50},,
+  {nombre : "silla de madera", precio : 80},
+  {nombre : "ventana", precio : 50},
+  {nombre : "puerta", precio : 90},
+];
+
+// forEach() => recorrer el array y ejecutar lo que le digamos con cada elemento
+
+// numeros.forEach((numero) => {
+//   console.log(numero);
+// }); 
+
+productos.forEach((producto) => {
+  console.log(producto.nombre);
+  console.log(producto.precio);
+});
 
