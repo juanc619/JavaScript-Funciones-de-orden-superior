af7dad579be3a4e1482dcf4e76520298e8312c06
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-06-12 21:07:35 -0500

    Math

--- main.js
+++ main.js
@@ -29,6 +29,17 @@
 
 /* Metodos de busqueda */
 
+const numeros = [50, 12, 37, 17, 25, 6];
+const animales = ["perro", "gato", "cisne", "tortuga"];
+const productos = [
+  {nombre : "mesa", precio : 35},
+  {nombre : "silla de plastico", precio : 50},,
+  {nombre : "silla de madera", precio : 80},
+  {nombre : "ventana", precio : 50},
+  {nombre : "puerta", precio : 90},
+];
+
+
 
 
 // forEach() => recorrer el array y ejecutar lo que le digamos con cada elemento
@@ -94,20 +105,49 @@
 
 // sort() => reordenar el array segun el criterio que le indiquemos
 
-const numeros = [50, 12, 37, 17, 25, 6];
-const animales = ["perro", "gato", "cisne", "tortuga"];
-const productos = [
-  {nombre : "mesa", precio : 35},
-  {nombre : "silla de plastico", precio : 50},,
-  {nombre : "silla de madera", precio : 80},
-  {nombre : "ventana", precio : 50},
-  {nombre : "puerta", precio : 90},
-];
 
-numeros.sort((a, b) => a - b);
-console.log(numeros);
+// numeros.sort((a, b) => a - b);
+// console.log(numeros);
 
-numeros.sort((a, b) => a + b);
-console.log(numeros);
+// numeros.sort((a, b) => a + b);
+// console.log(numeros);
 
 
+// MATH
+
+const numeroDecimal1 = 1.25;
+const numeroDecimal2 = 1.75;
+
+// // min() => retornar el numero minimo de un listado
+// console.log(Math.min(50, 12, 37, 17, 25, 6));
+
+// // max() => retorna el numero maximo de un listado
+// console.log (Math.max(50, 12, 37, 17, 25, 6));
+
+// // ceil () => retorna el numero que le indiquemos redondeado hacia arriba
+// console.log (Math.ceil(numeroDecimal1));
+// console.log (Math.ceil(numeroDecimal2));
+
+// // floor () => retorna el numero que le indiquemos redondeado hacia abajo
+// console.log (Math.floor(1.25));
+// console.log (Math.floor(1.75));
+
+// // round () => retorna el numero que le indiquemos redondeado mas cercano
+// console.log (Math.round(1.25));
+// console.log (Math.round(1.75));
+
+// random () => retorna un numero aleatorio entre 0 inclusive y 1 inclusive
+// console.log(Math.random());
+// console.log(Math.round(Math.random()));
+
+// console.log(Math.round(Math.random() * 50 ) );
+// console.log(Math.round(Math.random() * 50 + 20 ) );
+
+// console.log (Math.floor(Math.random() * 3 + 1 ));
+
+function generarNumero (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const numeroRandom = generarNumero(100, 200)
+console.log(numeroRandom);
